5c8e83952f332467cc6c46953222966a34589cb2
1amageek <norikazu-muramoto@cookpad.com>
2017-08-23 18:04:10 +0900

    Initial Commit

---
+++ Tong/Tong.h
@@ -0,0 +1,19 @@
+//
+//  Tong.h
+//  Tong
+//
+//  Created by nori on 2017/08/23.
+//  Copyright © 2017年 Stamp Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Tong.
+FOUNDATION_EXPORT double TongVersionNumber;
+
+//! Project version string for Tong.
+FOUNDATION_EXPORT const unsigned char TongVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Tong/PublicHeader.h>
+
+
